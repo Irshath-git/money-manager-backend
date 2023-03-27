35732a6d61447e775fc24d1ad78a469ebd70122f
Irshath-git <mrirshathbscit@gmail.com>
2023-03-27 20:22:12 +0530

    new commit

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 console.log(process.env.ATLAS_URI);
 const conn = mongoose
   .connect(process.env.ATLAS_URI)
+  // .set("strictQuery", false)
   .then((db) => {
     console.log("Database Connected");
     return db;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "./config.env" });
-
-const port = process.env.PORT || 4000;
+mongoose.set("strictQuery", false);
+const port = process.env.PORT || 5000;
 
 //using middlewares
 app.use(cors());
